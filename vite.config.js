791de156eaa8a6ade5c352810e796dbfbd0f50ef
Wellington Mota <wellingtonmota87@gmail.com>
2026-02-07 14:22:59 -0300

    Fix: Adjust base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Mil-avia-o-e-RBF-taxi-aereo/',
-  build: {
-    outDir: 'docs',
-  }
+  base: '/',
 })
